1d2485c83c8e5aed10633a7709a469da8ac9aa1c
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-15 19:04:14 +0700

    nav responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ function Nav() {
     return (
         <nav className="bg-slate-50">
            <div className="container mx-auto py-5 px-5 md:px-0">
-                <h1 className="font-semibold text-gray-800 text-xl text-center">Learning ReactJS</h1>
+                <h1 className="font-semibold text-gray-800 text-xl md:text-2xl text-center">Learning ReactJS</h1>
            </div>
         </nav>
     )
