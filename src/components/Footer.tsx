3de05df35de2188af14066ce6d4c5f25f7b95400
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-15 10:31:27 +0700

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="w-full">
+            <div className="container mx-auto py-5 px-5 md:px-0">
+                <h2 className="font-medium text-xl text-gray-500 text-center">From Yogyakarta With Love <span className="text-rose-600">❤</span></h2>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
