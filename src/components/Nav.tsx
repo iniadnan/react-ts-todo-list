3fb6081f3318514310300195fd18b174af4ba1b4
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-15 10:35:09 +0700

    add Nav.tsx

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav className="bg-slate-50">
+           <div className="container mx-auto py-5 px-5 md:px-0">
+                <h1 className="font-semibold text-gray-800 text-xl">Learning ReactJS</h1>
+           </div>
+        </nav>
+    )
+}
+
+export default Nav
