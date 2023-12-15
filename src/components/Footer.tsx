45c2b35ef957bbd637051654f8676e8ebc6ee7a6
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-15 19:01:25 +0700

    Footer text responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="w-full">
             <div className="container mx-auto py-5 px-5 md:px-0">
-                <h2 className="font-medium text-xl text-gray-500 text-center">From Yogyakarta With Love <span className="text-rose-600">❤</span></h2>
+                <h2 className="font-medium text-base md:text-lg lg:text-xl text-gray-500 text-center">From Yogyakarta With Love <span className="text-rose-600">❤</span></h2>
             </div>
         </footer>
     )
