2e6c4c754fa82abc64fef7ab3a05b428ed6e00ab
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-16 11:13:44 +0700

    delete & select todo

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ interface Todos {
 function App() {
 
   const [todos, setTodos] = useState<Todos[]>([])
+  const [todosSelected, setTodosSelected] = useState<Todos[]>([])
   const [newTodo, setNewTodo] = useState("")
   const [addTodo, setAddTodo] = useState(false)
 
@@ -44,6 +45,23 @@ function App() {
     setAddTodo(false)
   }
 
+  const deleteTodo = () => {
+    setTodos(todos.filter(todo => !todosSelected.some(todoSelected => todoSelected.id === todo.id)))
+  }
+
+  const onToggleSelect = (todo: { id: string, todo: string, status: string }) => {
+    const isTodoSelected = todosSelected.some(todoSelected => todoSelected.id === todo.id);
+    if (isTodoSelected) {
+      setTodosSelected(todosSelected.filter((todoSelected) => todoSelected.id !== todo.id));
+    } else {
+      setTodosSelected([...todosSelected, todo]);
+    }
+  }
+
+  const onToggleStatus = (id: string) => {
+    console.log(id)
+  }
+
   return (
     <>
       <Nav />
@@ -60,12 +78,12 @@ function App() {
               <InputNewTodo onInputChange={handleInput} />
             </form>
             <div className="flex flex-col gap-y-4 mt-8 mb-8 md:mb-10 lg:mb-12">
-              {todos.map((todo) => <ToDoCard key={todo.id} id={todo.id} todo={todo.todo} status={todo.status} />)}
+              {todos.map((todo) => <ToDoCard key={todo.id} id={todo.id} todo={todo.todo} status={todo.status} toggleSelect={onToggleSelect} toggleStatus={onToggleStatus} />)}
             </div>
             <div className="flex gap-x-3">
               <button onClick={toggleAddTodo} type="button" className="py-2 px-3 md:px-4 bg-blue-500 font-medium text-sm md:text-base text-white rounded">Add Todo</button>
               <button type="button" className={`${todos.length > 0 ? 'inline-block' : 'hidden'} py-2 px-3 md:px-4 font-medium text-base text-gray-700 rounded`}>Edit Todo</button>
-              <button type="button" className={`${todos.length > 0 ? 'inline-block' : 'hidden'} font-medium text-sm md:text-base text-rose-700 border-b border-rose-700 ml-auto`}>Delete Selected Todos</button>
+              <button onClick={deleteTodo} type="button" className={`${todos.length > 0 ? 'inline-block' : 'hidden'} font-medium text-sm md:text-base text-rose-700 border-b border-rose-700 ml-auto`}>Delete Selected Todos</button>
             </div>
           </div>
         </div>
--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -1,22 +1,40 @@
+import { useState } from "react"
+
 interface Props {
     id: string,
     todo: string,
     status: string
+    toggleSelect: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    toggleStatus: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 function ToDoCard(props: Props) {
 
-    const { id, todo, status } = props
+    const { id, todo, status, toggleSelect, toggleStatus } = props
+
+    const [checkIcon, setCheckIcon] = useState(false)
+
+
+    const onToggleSelect = () => {
+        setCheckIcon(prevState => {
+            return !prevState
+        })
+        toggleSelect({ id: id, todo: todo, status: status })
+    }
+
+    const onToggleStatus = () => {
+        toggleStatus(id)
+    }
 
     return (
         <article id={id} className="w-full bg-slate-100 py-5 px-5 rounded flex items-center gap-x-3">
-            <button type="button" className="h-5 w-5 rounded bg-slate-300 inline-flex items-center justify-center flex-none text-transparent">
+            <button onClick={onToggleSelect} type="button" className={`${checkIcon ? 'bg-blue-500 text-white' : 'bg-slate-300 text-transparent'} h-5 w-5 rounded inline-flex items-center justify-center flex-none`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                 </svg>
             </button>
-            <h3 className="font-medium text-base text-gray-700 grow">{ todo }</h3>
-            <button type="button" className={`${status == 'complete' ? 'bg-green-600' : 'bg-gray-400'} inline-block py-2 px-4 rounded-md text-xs text-white font-medium flex-none uppercase`}>{ status }</button>
+            <h3 className="font-medium text-base text-gray-700 grow">{todo}</h3>
+            <button onClick={onToggleStatus} type="button" className={`${status == 'complete' ? 'bg-green-600' : 'bg-gray-400'} inline-block py-2 px-4 rounded-md text-xs text-white font-medium flex-none uppercase`}>{status}</button>
         </article>
     )
 }
